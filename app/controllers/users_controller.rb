97676fc8ce1201be229f8723eda1c9db6dc27e96
teresa-lo <teresa@financeit.io>
2019-01-06 18:03:37 -0500

    Current user API: access the current valid user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,16 @@
 class UsersController < ApplicationController
 
+  # In header pass the Authorization key with token value "Bearer Token"
+  # Token can be obtained by hitting session API of that user
+  #
+  def show_current
+    authenticate_or_request_with_http_token do |token, options|
+      verify_token = JWT.decode token, JWT_SECRET, true, {algorithm: "HS512"}
+      user_id = verify_token[0]["sub"]
+      puts user_id
+      @current_user = User.find(user_id)
+    end
+
+    render json: @current_user
+  end
 end
